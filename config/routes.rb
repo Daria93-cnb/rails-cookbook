b03fb4f04c8c2420c860e36882a9ae3c31c0a1a8
Daria Potopnic <daria.cnb@gmail.com>
2025-05-13 22:14:54 +0100

    Implemented List of Categories in CookBook

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "categories#index"
+
   get 'bookmarks/new'
   get 'bookmarks/create'
   get 'bookmarks/destroy'
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+
   get "up" => "rails/health#show", as: :rails_health_check
 
     resources :categories, only: [:index, :show, :new, :create] do
